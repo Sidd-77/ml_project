48261bc33dd0d86954af661a78be9e69e5c05d01
Sidd-77 <siddharthsalunkhe2003@gmail.com>
2024-03-15 03:14:15 +0530

    intial setup and requirements

---
+++ test.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def test(fpath:str)->List[str]:
+    requirements =[]
+    with open(fpath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+    return requirements
+
+too = test('requirements.txt')
+print(too)
